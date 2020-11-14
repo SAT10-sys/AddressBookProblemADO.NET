df0aa04bfc10f7524e61252e05f0d71d05956e4d
Satantra Tewari <satantra1508@gmail.com>
2020-11-15 01:00:41 +0530

    [satantra] test project added

---
+++ AddressBookTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using AddressBookProblem.ADONET;
+
+namespace AddressBookTest
+{
+    public class tests
+    {
+        [SetUp]
+        public void SetUp()
+        { }
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
